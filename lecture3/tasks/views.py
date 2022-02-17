a0b762878b312d28afe2b78d32ce80088046a6ff
Donaldcho <donaldcho28@gmail.com>
2022-02-17 20:29:07 +0400

    Donald added newyear app, task app

---
+++ lecture3/tasks/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django import forms
+
+# Create your views here.
+from django.shortcuts import render
+
+tasks = ["foo", "bar", "baz"]
+
+# Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
+
+
+# Add a new task:
+def add(request):
+    return render(request, "tasks/add.html")
+
